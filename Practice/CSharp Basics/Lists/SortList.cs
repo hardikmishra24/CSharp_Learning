4053c92161d582b7d7f43e28b84f9134e4a6b248
Hardik Mishra <hardikmishra2024@gmail.com>
2026-06-14 13:23:54 +0530

    Implement Run method to display sorted students

--- Practice/CSharp Basics/Lists/SortList.cs
+++ Practice/CSharp Basics/Lists/SortList.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.CSharp_Basics.Lists
 {
-    internal class SortList
-    { sorted new = sorted()
-    }
+    public static void Run()
+ {
+     SortedList students = new SortedList();
+
+     students.Add(3, "Rahul");
+     students.Add(1, "Hardik");
+     students.Add(2, "Aman");
+
+     foreach (DictionaryEntry student in students)
+     {
+         Console.WriteLine(student.Key + " " + student.Value);
+     }
+  }
 }
