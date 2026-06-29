56640e1c3e19b3042db96c10bf7121f37f6e6b0a
Hardik Mishra <hardikmishra2024@gmail.com>
2026-06-29 23:48:33 +0530

    Added Extension Methods

--- Practice/CSharp Basics/ExtensionMethods/IntExtension.cs
+++ Practice/CSharp Basics/ExtensionMethods/IntExtension.cs
@@ -9,9 +9,9 @@ namespace ConsoleApp1.CSharp_Basics.ExtensionMethods
 
     // Extension methods are methods that let you add new functionality(methods) to an existing class without modifying its source code or creating a derived class.
     // For example adding a method in a predefined class like string or int.
-            public static class IntExtension  // The class must be static
+            public static class IntExtension  // The class must be static.
         {
-            public static bool IsGreaterThan(this int i, int value)
+            public static bool IsGreaterThan(this int i, int value) // The method made must be static and the parameter inside the method must be of the same type on which the extension method is made in this case (int) with a this keyword in the starting.
             {
                 return i > value;
             }
--- Practice/CSharp Basics/ExtensionMethods/Size.cs
+++ Practice/CSharp Basics/ExtensionMethods/Size.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsoleApp1.CSharp_Basics.ExtensionMethods;
+using ConsoleApp1.CSharp_Basics.ExtensionMethods;  // Here we have to include the namespace of the extension method where the method was made to use the method.
 namespace ConsoleApp1.CSharp_Basics.ExtensionMethods
 {
     internal class Size
@@ -12,7 +12,7 @@ namespace ConsoleApp1.CSharp_Basics.ExtensionMethods
         {
             int i = 10;
 
-            bool result = i.IsGreaterThan(100);
+            bool result = i.IsGreaterThan(100);  // Implementing the extension method
 
             Console.WriteLine("Result :" + result);
         }
