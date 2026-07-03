1cd6a99e108c1d967c1c605bf699812debb61d4a
Hardik Mishra <hardikmishra2024@gmail.com>
2026-07-03 23:35:18 +0530

    Updated Solid Principles

---
+++ Practice/CSharp Basics/SOLID_Principles/Open-Closed_Principle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.CSharp_Basics.SOLID_Principles
+{
+    // It states that a class should be open for extension but closed for modifications other than fixing bugs.
+    // We should be able to add new functionality but cannot modify the existing codebase.
+    // Can be implemented using polymorphism, inheritance, function parameters or abstract classes.
+
+    interface Inotification
+    {
+        void Send();
+    }
+
+    public class Email : Inotification
+    {
+        public void Send()
+        {
+            Console.WriteLine("Email send");
+        }
+
+        public class Whatsapp : Inotification
+        {
+            public void Send()
+            {
+                Console.WriteLine("Whatsapp send");
+            }
+        }
+
+        internal class Open_Closed_Principle
+        {
+            public static void Run()
+            {
+                Inotification notification = new Email();
+                notification = new Whatsapp();
+                // Here if we want to add new service like telegram we can add easily without modifying the code just a new class which extends the interface is required.
+            }
+        }
+    }
+}
--- Practice/CSharp Basics/SOLID_Principles/Single_Responsibility_Principle.cs
+++ Practice/CSharp Basics/SOLID_Principles/Single_Responsibility_Principle.cs
@@ -6,7 +6,105 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.CSharp_Basics.SOLID_Principles
 {
+    // Single Responsibility Principle states that a class should have only one Responsibility and therefore it should have only reason to change the code.
+    // Seperation of Concerns Principle is also followed here it suggests that the application should be separated into distinct sections where each section addresses a separate concern or set of information that affects the program.
+
+
     internal class Single_Responsibility_Principle
     {
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string FirstName { get; set; }
+            public string Email { get; set; }
+
+            public void Save()
+            {
+                Logger.Log("Starting Save()");
+
+                // Save student to database
+
+                Logger.Log("End Save()");
+            }
+
+            public void Delete()
+            {
+                Logger.Log("Starting Delete()");
+
+                // Delete student from database
+
+                Logger.Log("End Delete()");
+            }
+        }
+
+        public class Logger
+        {
+            public static void Log(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public class StudentRepository
+        {
+            public bool Save(Student std)
+            {
+                Logger.Log("Starting Save()");
+
+                // Use EF to add or update student
+
+                Logger.Log("Ending Save()");
+                return true;
+            }
+
+            public bool Delete(Student std)
+            {
+                Logger.Log("Starting Delete()");
+
+                // Use EF to delete student
+
+                Logger.Log("Ending Delete()");
+                return true;
+            }
+
+            public bool SaveCourse(Student std, Course cs)
+            {
+                Logger.Log("Starting SaveCourse()");
+
+                // Save student-course relation
+
+                Logger.Log("Ending SaveCourse()");
+                return true;
+            }
+        }
+
+        public class Course
+        {
+            public int CourseId { get; set; }
+            public string Title { get; set; }
+            public string Type { get; set; }
+
+            public void Subscribe(Student std)
+            {
+                Logger.Log("Starting Subscribe()");
+
+                if (Type == "online")
+                {
+                    // Subscribe to online course
+                }
+                else if (Type == "live")
+                {
+                    // Subscribe to live course
+                }
+
+                // Payment processing
+
+                // Save student-course relation
+
+                // Send confirmation email
+
+                Logger.Log("End Subscribe()");
+            }
+        }
     }
 }
